185c03ecd74d861c665540c36aaef70c6345d663
Doreen <doreenmarie_cee@hotmail.com>
2024-01-19 11:10:31 +0100

    set-up UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import RegisterPage from './components/RegisterPage';
-import LoginForm from '/src/components/LoginForm';
+import LoginForm from './components/LoginForm';
+import { UserProvider } from './contexts/UserContext';
 
 
 function App () {
@@ -10,16 +11,20 @@ function App () {
 
   return (
     <Router>
+    <UserProvider>
       <Routes>
         <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/login" element={<LoginForm onLoginSuccess={handleLoginSuccess} />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
+    </UserProvider>
     </Router>
   );
 }
   
 
 export default App;
+
+
 
 
---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,52 @@
+import { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+
+
+// create a context
+export const UserContext = createContext();
+
+// create a provider component
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+
+  useEffect(() => {
+
+    // function to fetch user data from the API
+    // getCurrentUserId() is a helper function defined below
+    const currentUserId = getCurrentUserId()
+    
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5005/user/${currentUserId}`); 
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+
+
+
+// a helper function to get the current user id from local storage
+// this is used to fetch the user data from the API
+function getCurrentUserId() {
+  return localStorage.getItem('userId');
+}
+
+UserProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+  
