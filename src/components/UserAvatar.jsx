9d3ec6554951a3c6ece14988ccfc928bdf9a90cd
Doreen <doreenmarie_cee@hotmail.com>
2024-01-21 01:15:39 +0100

    adding css UserProfile

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -5,8 +5,8 @@ import profilePic from "../assets/samprofPic.jpg"
 function UserAvatar() {
   return (
     <div>
-   <div className="rounded-full w-12 overflow-hidden">
-   <img src={profilePic} alt="Profile" />
+   <div className="rounded-full w-14 h-14 overflow-hidden">
+   <img src={profilePic} alt="Profile" className="w-full h-full object-cover" />
    </div>
    
    </div>
