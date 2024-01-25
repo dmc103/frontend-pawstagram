4c8b4a13e8ae5f8fa525ae22943f1f52161d1900
Doreen <doreenmarie_cee@hotmail.com>
2024-01-25 20:09:17 +0100

    modified card component

--- src/components/SubComponents/Card.jsx
+++ src/components/SubComponents/Card.jsx
@@ -1,22 +1,18 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-
-
-
-function Card( {children} ) {
+function Card({ children }) {
   return (
-    <div className="bg-white shadow-md shadow-gray-300 rounded-md p-4 mb-5">
-    {children}
+    <div
+      className="bg-white shadow-md shadow-gray-300 rounded-md p-4 mb-5"
+      style={{ boxShadow: "1px 0 15px -3px rgba(0, 0, 0, 0.3)" }}
+    >
+      {children}
     </div>
-  )
+  );
 }
 
-
-
-
-export default Card
-
+export default Card;
 
 Card.propTypes = {
-    children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired,
+};
