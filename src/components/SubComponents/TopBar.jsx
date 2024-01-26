5205dfa6c49fa203dc366530a9a5e8f6258e90dc
Doreen <doreenmarie_cee@hotmail.com>
2024-01-26 21:30:45 +0100

    new components

--- src/components/SubComponents/TopBar.jsx
+++ src/components/SubComponents/TopBar.jsx
@@ -1,70 +1,74 @@
 import "ionicons";
 import pawstagram from "../../assets/pawstagram.png";
 import UserAvatar from "./UserAvatar";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import { useContext } from "react";
 
 function TopBar() {
-  return (
-    <div className="w-full border-gray-200 px-4 sm:px-6 lg:px-8 bg-pawBgFour">
-      <div className="flex items-center justify-between h-16">
-        {/* Logo Section */}
-        <div className="flex">
-          <img
-            src={pawstagram}
-            className="h-14 w-14 m-4"
-            alt="Pawstagram logo"
-          />
-        </div>
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
-        <div className="flex-1 hidden md:block">
-          <div className="relative">
-            <input
-              type="search"
-              className="w-1/2 pr-10 pl-4 py-2 border rounded-md leading-5  border-pawBgTwo dark:text-gray-500 focus:outline-none focus:ring-2 focus:ring-pawBgOne focus:border-pawBgOne"
-              placeholder="Search..."
-            />
-            <div className="absolute inset-y-0 right-14 pr-14 flex items-center pointer-events-none">
-              {/* Search Icon */}
-              <button
-                type="button"
-                className="p-1 border-2 border-transparent text-gray-600 rounded-full hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-200 dark:focus:bg-gray-800 transition duration-150 ease-in-out cursor-pointer"
-                aria-label="Logout"
-              >
-                <ion-icon name="search-outline"></ion-icon>
-              </button>
-              <div className="mt-7 pb-5">
-                <UserAvatar
-                  isOnline={true}
-                  size="w-11 h-11"
-                  indicatorPosition="top-6 left-9"
-                />
-              </div>
-            </div>
-          </div>
+  return (
+    <div className="w-full flex items-center border-gray-200 px-4 sm:px-6 lg:px-8 bg-pawBgFour">
+      {/* Logo Section */}
+      <div className={"flex-shrink-0" + theme}>
+        <button className="theme-btn" onClick={toggleTheme}>
+          <img src={pawstagram} className="h-14 w-14" alt="Pawstagram logo" />
+        </button>
+      </div>
+
+      {/* Search Input */}
+      <div className="flex-1 flex justify-center items-center">
+        <div className="relative w-1/3">
+          <input
+            type="search"
+            className="pl-4 pr-10 py-2 border rounded-md leading-5 bg-white dark:text-gray-500 focus:outline-none focus:ring-2 focus:ring-pawBgOne focus:border-pawBgOne"
+            placeholder="Search..."
+          />
+          <button
+            type="button"
+            className="absolute inset-y-0 right-6 flex items-center text-gray-600 hover:text-gray-700 focus:outline-none"
+            aria-label="Search"
+          >
+            <ion-icon
+              name="search-outline"
+              style={{ fontSize: "1.5em" }}
+            ></ion-icon>
+          </button>
         </div>
+      </div>
+
+      {/* Avatar, Notifications, and Logout Button */}
+      <div className="flex items-center gap-4">
+        {/* UserAvatar */}
+        <UserAvatar
+          isOnline={true}
+          size="w-11 h-11"
+          indicatorPosition="bottom-0 right-0"
+        />
 
         {/* Notifications Button */}
-
-        <div className="ml-4 flex items-center md:ml-6">
-          <button
-            type="button"
-            className="p-1 border-2 border-transparent text-gray-600 rounded-full hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-200 dark:focus:bg-gray-800 transition duration-150 ease-in-out"
-            aria-label="Logout"
-          >
-            <ion-icon name="notifications-outline"></ion-icon>
-          </button>
-        </div>
+        <button
+          type="button"
+          className="p-1 border-2 border-transparent text-gray-600 rounded-full hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-200 dark:focus:bg-gray-800 transition duration-150 ease-in-out"
+          aria-label="Notifications"
+        >
+          <ion-icon
+            name="notifications-outline"
+            style={{ fontSize: "1.5em" }}
+          ></ion-icon>
+        </button>
 
         {/* Logout Button */}
-
-        <div className="ml-4 flex items-center md:ml-6">
-          <button
-            type="button"
-            className="p-1 border-2 border-transparent text-gray-600 rounded-full hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-200 dark:focus:bg-gray-800 transition duration-150 ease-in-out cursor-pointer"
-            aria-label="Logout"
-          >
-            <ion-icon name="log-out-outline"></ion-icon>
-          </button>
-        </div>
+        <button
+          type="button"
+          className="p-1 border-2 border-transparent text-gray-600 rounded-full hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-200 dark:focus:bg-gray-800 transition duration-150 ease-in-out"
+          aria-label="Logout"
+        >
+          <ion-icon
+            name="log-out-outline"
+            style={{ fontSize: "1.5em" }}
+          ></ion-icon>
+        </button>
       </div>
     </div>
   );
--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import NavBar from "./SubComponents/NavBar";
 import CardForPost from "./SubComponents/CardForPost";
 import CardForSharedPost from "./SubComponents/CardForSharedPost";
-import UserAvatar from "./SubComponents/UserAvatar";
 import { ThemeContext } from "../contexts/ThemeContext";
 import TopBar from "./SubComponents/TopBar";
 
@@ -28,52 +27,18 @@ function UserHome() {
   }
 
   return (
-    <div className="home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-pawBgTwo lg:rounded-lg h-screen overflow-hidden">
+    <div>
       <TopBar />
-      <div className={"flex mt-4 max-w-4xl mx-auto gap-6 " + theme}>
-        <NavBar />
-        <div className={"w-3/4 " + theme}>
-          <div className={theme}>
-            <div
-              className={
-                "w-full max-w-XL bg-white border border-gray-200 rounded-lg shadow dark:bg-pawBgFour dark:border-emerald-300 color-bg " +
-                theme
-              }
-            >
-              <div className="flex flex-col items-center mt-7 pb-10">
-                <UserAvatar isOnline={true} />
-
-                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                  {user.userName}
-                </h5>
-
-                <span className="text-sm text-gray-500 dark:text-gray-100">
-                  {user.bio}
-                </span>
-
-                <div className="flex mt-4 md:mt-6">
-                  <a
-                    href="#"
-                    className={
-                      "inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white  bg-indigo-600  rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 button " +
-                      theme
-                    }
-                  >
-                    Add friend
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3"
-                  >
-                    Message
-                  </a>
-                </div>
-              </div>
-            </div>
+      <div className="w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-pawBgTwo lg:rounded-lg h-screen overflow-hidden">
+        <div className="flex">
+          <div className={"flex mt-4 max-w-4xl mx-auto " + theme}>
+            <NavBar />
           </div>
 
-          <CardForPost />
-          <CardForSharedPost />
+          <div className="flex flex-col">
+            <CardForPost />
+            <CardForSharedPost />
+          </div>
         </div>
       </div>
     </div>
