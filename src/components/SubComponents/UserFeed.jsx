648a083b94c243431ea0280e5642a765c19accc1
Doreen <doreenmarie_cee@hotmail.com>
2024-01-28 13:12:32 +0100

    modify user profile

---
+++ src/components/SubComponents/UserFeed.jsx
@@ -0,0 +1,7 @@
+import "./UserFeed.css";
+
+function UserFeed() {
+  return <div>UserFeed</div>;
+}
+
+export default UserFeed;
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -7,6 +7,7 @@ import banner from "../assets/banner.jpg";
 import UserAvatar from "./SubComponents/UserAvatar";
 import Tab from "./SubComponents/Tab";
 import { ThemeContext } from "../contexts/ThemeContext";
+import TopBar from "./SubComponents/TopBar";
 
 function UserProfile() {
   const { userName } = useParams();
@@ -25,36 +26,40 @@ function UserProfile() {
   }
 
   return (
-    <div className={"flex mt-4 max-w-4xl mx-auto gap-6 color-bg " + theme}>
-      <div className={theme}>
-        <Nav />
+    <div>
+      <TopBar />
+
+      <div className={"flex mt-4 max-w-4xl mx-auto gap-6 color-bg " + theme}>
+        <div className={theme}>
+          <Nav />
+        </div>
+
+        <Card>
+          <div className="relative">
+            <div className="flex rounded-lg overflow-hidden justify-center items-center h-60">
+              <img src={banner} alt="banner" />
+            </div>
+
+            <div className="absolute bottom-0 transform translate-y-1/2 w-full flex justify-center">
+              <UserAvatar
+                isOnline={true}
+                size="w-20 h-20"
+                indicatorPosition="top-14 start-16"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-center items-center h-44">
+            <div className="max-w-lg text-xl font-bold leading-normal text-pawBgFour mt-2 mb-2">
+              <p>{userName}</p>
+            </div>
+            <div className="text-m text-gray-500">{user.country}</div>
+            <div className="text-m text-gray-500">{user.bio}</div>
+          </div>
+
+          <Tab />
+        </Card>
       </div>
-
-      <Card>
-        <div className="relative">
-          <div className="flex rounded-lg overflow-hidden justify-center items-center h-60">
-            <img src={banner} alt="banner" />
-          </div>
-
-          <div className="absolute bottom-0 transform translate-y-1/2 w-full flex justify-center">
-            <UserAvatar
-              isOnline={true}
-              size="w-20 h-20"
-              indicatorPosition="top-14 start-16"
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-col justify-center items-center h-44">
-          <div className="max-w-lg text-xl font-bold leading-normal text-pawBgFour mt-2 mb-2">
-            <p>{userName}</p>
-          </div>
-          <div className="text-m text-gray-500">{user.country}</div>
-          <div className="text-m text-gray-500">{user.bio}</div>
-        </div>
-
-        <Tab />
-      </Card>
     </div>
   );
 }
