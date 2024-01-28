aeb4287440d3a5a361d672ecf81b12d917c6236e
Dinah Salazar <dinahsalazar@gmail.com>
2024-01-28 12:24:48 +0900

    css PasswordResetPage

--- src/components/PasswordResetPage.jsx
+++ src/components/PasswordResetPage.jsx
@@ -1,10 +1,33 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 function PasswordResetPage() {
     const [oldPassword, setOldPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [passwordStatus, setPasswordStatus] = useState("");
 
+    const navigate = useNavigate();
+
+    const handleResetPassword = (e) => {
+        e.preventDefault();
+        console.log("handleReset");
+        if (newPassword === confirmPassword) {
+            setPasswordStatus("Password reset. Redirecting to login page...");
+            setTimeout(() => {
+                navigate("login");
+            }, 3000);
+        } else {
+            setPasswordStatus("Passwords didn't match. Please try again.");
+        }
+        
+    }
+
+    const handleBack = (e) => {
+        e.preventDefault();
+        console.log("handleBack");
+        navigate("/login");
+    }
   return (
     <div className="bg-pawBgFour h-screen flex flex-col items-center justify-center">
         <h2 className="text-center text-white mb-12 font-medium text-3xl mt-12 hover:text-indigo-800">PASSWORD RESET</h2>
@@ -33,9 +56,9 @@ function PasswordResetPage() {
                 onChange={(e) => {setNewPassword(e.target.value)}}/>
             </label>
 
-            <label htmlFor="new-pw">
+            <label htmlFor="confirm-pw">
                 <input 
-                className="w-full p-3 border border-slate-700 bg-amber-50 rounded-2xl mb-10  hover:bg-amber-200"
+                className="w-full p-3 border border-slate-700 bg-amber-50 rounded-2xl mb-2 hover:bg-amber-200"
                 placeholder="Confirm New Password"
                 type="password" 
                 name="confirm-pw" 
@@ -43,11 +66,14 @@ function PasswordResetPage() {
                 value={confirmPassword} 
                 onChange={(e) => {setConfirmPassword(e.target.value)}}/>
             </label>
+
+            <p className="text-red-600 font-medium blink_me">{passwordStatus}</p>
+
             <br />
             <button
-            className="h-10 bg-gradient-to-r from-sky-400 to-indigo-600 w-300 rounded-2xl mb-14 hover:text-white hover:h-12">Reset Password</button>
+            className="mt-5 h-10 bg-gradient-to-r from-sky-400 to-indigo-600 w-300 rounded-2xl mb-14 hover:text-white hover:h-12 border-2 border-amber-100" onClick={handleResetPassword}>Reset Password</button>
             <button
-        className="h-10 bg-gradient-to-r from-sky-400 to-indigo-600 w-300 rounded-2xl mb-8 hover:text-white hover:h-12">Back</button>
+        className="h-10 bg-gradient-to-r from-sky-400 to-indigo-600 w-300 rounded-2xl mb-8 hover:text-white hover:h-12 border-2 border-amber-100" onClick={handleBack}>Back</button>
         </form>
         </div>
         
