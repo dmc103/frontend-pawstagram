2eba21ba1b5d4f1cc14626c4923e6316049c7e14
Dinah Salazar <dinahsalazar@gmail.com>
2024-02-04 00:58:16 +0900

    theme

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -26,10 +26,10 @@ function UserProfile() {
   }
 
   return (
-    <div className={"dark-bg " + theme}>
+    <div className={"color-bg " + theme}>
       <TopBar />
 
-      <div className={"flex mt-4 max-w-4xl mx-auto gap-6 dark-bg " + theme}>
+      <div className={"flex mt-4 max-w-4xl mx-auto gap-6 color-bg " + theme}>
         <div>
           <Nav />
         </div>
