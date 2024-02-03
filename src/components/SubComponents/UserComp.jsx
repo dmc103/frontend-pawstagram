0ce5bc4995284a3c5bf8729ebf5138a436de5ddc
Doreen <doreenmarie_cee@hotmail.com>
2024-02-03 12:11:52 +0100

    adding card

--- src/components/SubComponents/UserComp.jsx
+++ src/components/SubComponents/UserComp.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import UserAvatar from "./UserAvatar";
+import Card from "./Card";
 
 function UserComp({ user, currentUser, onAddFriend }) {
   const isFriend =
@@ -8,37 +9,39 @@ function UserComp({ user, currentUser, onAddFriend }) {
     currentUser.following.includes(user._id);
 
   return (
-    <div className="group before:hover:scale-95  before:hover:w-80 before:hover:h-44 before:hover:rounded-b-xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-orange-200 to-orange-700 before:absolute before:top-0 w-50 h-42 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden mb-5">
-      <div className="relative w-20 h-20 mt-8">
-        <div className="absolute inset-0 bg-pawBgFour rounded-full border-4 border-slate-50 group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500 z-0"></div>
+    <Card>
+      <div className="group before:hover:scale-95  before:hover:w-80 before:hover:h-44 before:hover:rounded-b-xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-orange-200 to-orange-700 before:absolute before:top-0 w-50 h-42 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden mb-5">
+        <div className="relative w-20 h-20 mt-8">
+          <div className="absolute inset-0 bg-pawBgFour rounded-full border-4 border-slate-50 group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500 z-0"></div>
 
-        <div className="absolute inset-0 z-10">
-          <UserAvatar
-            profileImageUrl={user.profilepic}
-            isOnline={true}
-            size="w-20 h-20"
-            indicatorPosition="absolute top-14 left-16"
-            className="group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500"
-          />
+          <div className="absolute inset-0 z-10">
+            <UserAvatar
+              profileImageUrl={user.profilepic}
+              isOnline={true}
+              size="w-20 h-20"
+              indicatorPosition="absolute top-14 left-16"
+              className="group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500"
+            />
+          </div>
         </div>
-      </div>
 
-      <span className="text-md font-semibold">{user.userName}</span>
-      <div className="z-10  group-hover:-translate-y-10 transition-all duration-500">
-        <p className="text-sm text-gray-600 w-60 before:hover:h-44">
-          {user.bio}
-        </p>
-      </div>
+        <span className="text-md font-semibold">{user.userName}</span>
+        <div className="z-10  group-hover:-translate-y-10 transition-all duration-500">
+          <p className="text-sm text-gray-600 w-60 before:hover:h-44">
+            {user.bio}
+          </p>
+        </div>
 
-      {!isFriend && (
-        <button
-          className="bg-blue-700 px-4 py-1 text-slate-50 rounded-md z-10 hover:scale-125 transition-all duration-500 hover:bg-blue-500"
-          onClick={() => onAddFriend(user._id)}
-        >
-          Follow
-        </button>
-      )}
-    </div>
+        {!isFriend && (
+          <button
+            className="bg-blue-700 px-4 py-1 text-slate-50 rounded-md z-10 hover:scale-125 transition-all duration-500 hover:bg-blue-500"
+            onClick={() => onAddFriend(user._id)}
+          >
+            Follow
+          </button>
+        )}
+      </div>
+    </Card>
   );
 }
 
